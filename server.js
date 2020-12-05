cb5f96059af3b02001b8db65a8621b98bd154db8
Zhibek <zhibek.abay@yahoo.de>
2020-12-05 23:51:18 +0100

    ready for heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( 'MyCookieBook/src/Frontend/FE_ProfilePage/dist/angular-de-tutorial'));
+app.use(express.static( __dirname + '/dist/angular-de-tutorial'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('MyCookieBook/src/Frontend/FE_ProfilePage/dist/angular-de-tutorial/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-de-tutorial/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
