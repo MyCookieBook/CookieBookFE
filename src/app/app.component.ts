ba9ea0d780bd8ee39849fac883e6d6da29d96eb4
sarah1410 <sarah.glatt1@gmail.com>
2021-05-11 09:20:17 +0200

    Delete unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,34 +16,4 @@ export class AppComponent {
       this.id = params['userId']
       });
   }*/
-
-  /*
-  hideProfilePage;
-  hideLoginPage;
-  hideMainPage;
-
-  constructor() {
-    this.hideProfilePage = true;
-    this.hideLoginPage = false;
-    this.hideMainPage = true;
-  }
-
-  showProfilePage() {
-    this.hideProfilePage = false;
-    this.hideLoginPage = true;
-    this.hideMainPage = true;
-  }
-
-  showLoginPage() {
-    this.hideProfilePage = true;
-    this.hideLoginPage = false;
-    this.hideMainPage = true;
-  }
-
-  showMainPage() {
-    this.hideProfilePage = true;
-    this.hideLoginPage = true;
-    this.hideMainPage = false;
-  }
-  */
 }
