05bc07cee9a18c89a0806438842916eed5343e0a
zhibekbastian <zhibek.abay@yahoo.de>
2021-06-13 17:46:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( __dirname + '/dist/angular-de-tutorial'));
+      //app.use(express.static( __dirname + '/dist/angular-de-tutorial'));
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
 
 app.get('/*', function(req,res) {
     
