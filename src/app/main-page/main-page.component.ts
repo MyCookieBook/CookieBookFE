9fb919f79ff11607f58e09fb860e73c30405c799
sinja742 <3pt5grfdYF4>
2021-06-14 18:07:01 +0200

    merged master into dev

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Observable} from 'rxjs';
 
@@ -9,19 +9,12 @@ import {Observable} from 'rxjs';
 })
 export class MainPageComponent /*implements OnInit*/ {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
-
     var userId = localStorage.getItem('UserID');
     if (userId === null) {
-      this.router.navigate(['/login']);
-    }
-
-  }
-  ngOnInit(): void {
-    var userId = localStorage.getItem('UserID');
-    if(userId === null){
       this.router.navigate(['/login']);
     }
   }
