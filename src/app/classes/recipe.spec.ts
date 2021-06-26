f4a1699a55242b2e9ad33e7294f699ce96e5316c
Zhibek <zhibek.abay@yahoo.de>
2021-06-26 23:30:28 +0200

    more recipe tests

--- src/app/classes/recipe.spec.ts
+++ src/app/classes/recipe.spec.ts
@@ -15,7 +15,20 @@ describe('Recipe', () => {
   });
   it('should set an empty recipe', () => {
   let recipe = new Recipe();
-  recipe.setRecipe("§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&undefined&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§")
+  recipe.setRecipe("§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§&&&§§§")
     expect(recipe.getId()).toEqual(0);
+    expect(recipe.getCategory()).toEqual('OTHER');
+    expect(recipe.getSubcategory()).toEqual('DEFAULT');
+    expect(recipe.getTitle()).toEqual('');
+    expect(recipe.getBookmark()).toEqual(false);
+    expect(recipe.getAuthor()).toEqual('');
+    expect(recipe.getDifficulty()).toEqual(1);
+    expect(recipe.getDuration()).toEqual(0);
+    expect(recipe.getCalory()).toEqual('');
+    expect(recipe.getLink()).toEqual('');
+    expect(recipe.getOther()).toEqual('');
+    expect(recipe.getMaterial()).toEqual([]);
+    expect(recipe.getIngredient()).toEqual([]);
+    expect(recipe.getStep()).toEqual([]);
   });
 });
